48e6dc8017ca23bac6bd38f23f7683c32259f576
Bhavya Sri Pallem <palbhavy@sutherlandglobal.com>
2025-11-04 13:57:26 +0530

    checkout function

--- vars/wel.groovy
+++ vars/wel.groovy
@@ -3,3 +3,13 @@ def call(String projectName) {
         echo "welcome to: ${projectName.toUpperCase()}"
     }
 }
+
+
+def call(Map stageParams) {
+
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+  }
